a0f1731492d0057f26fa3f914bbd00b69733e9cc
michellenaim <michellenaim@gmail.com>
2020-11-03 12:11:30 -0800

    Add styling to slider

---
+++ sounds.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
+})
+
+// <a onclick="myAudioFunction('a');">
+//         <img src="icons/bird.png">
+//     </a>
+//     <a onclick="myAudioFunction('b');">
+//         <img src="icons/cat.png">
+//     </a>
+//     <script>
+//         var aAudio = new Audio('/sounds/birds.mp3');
+//         var bAudio = new Audio('/sounds/cat_purring.mp3');
+//         function myAudioFunction(letter) {
+//             if (letter == 'a') {
+//                 aAudio.play();
+//             } else if (letter == 'b') {
+//                 bAudio.play();
+//             }
+//         }
+//     </script>

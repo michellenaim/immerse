f06e23b720c868e4b945d53895da67cd3bcb29c7
michellenaim <michellenaim@gmail.com>
2020-11-04 18:57:42 -0800

    Add styling to nav bar

--- sounds.js
+++ sounds.js
@@ -1,46 +1,29 @@
-let audio, playbtn, volumeslider
+// PLAY PAUSE SOUNDS LINKED TO IMG & SLIDER
 
 function initAudioPlayer(){
-	// audio = new Audio();
-	// audio.src = "sounds/birds.mp3";
-	// audio.loop = true;
-
-    // sounds = 
-
-    // playbtn = document.getElementsByClassName("play");
-	// volumeslider = document.getElementsByClassName("volume_bar");
-
-	// function playPause(){
-	// 	if(audio.paused){
-	// 	    audio.play();
-	//     } else {
-	// 	    audio.pause();
-	//     }
-	// }
-	// function setvolume(){
-	//     audio.volume = volumeslider.value / 100;
-    // }
     icons = document.getElementsByClassName("icon");
     for (let i = 0; i < icons.length; i++){
-        icon = icons[i]
+        let icon = icons[i];
         let audio = icon.nextElementSibling;
         let volumeSlider = audio.nextElementSibling;
-
+        
+        // Play/pause audio on click
         icon.addEventListener("click", function(event) {
             if (audio.paused) {
                 audio.loop = true;
-                volumeSlider.value = 0.2 
+                volumeSlider.value = 0.1
                 audio.volume = volumeSlider.value
                 audio.play();
-
+                icon.children[0].classList.add('playing')
+                volumeSlider.classList.add('playing')
             } else {
                 audio.pause();
-            }
+                icon.children[0].classList.remove('playing')
+                volumeSlider.classList.remove('playing')            }
         });
 
-        // grab volume slider from icon, sibling
+        // Set up slider
         volumeSlider.addEventListener('input', function(event) {
-            // console.log(event.currentTarget)
             console.log(event.currentTarget.value)
             audio.volume = event.currentTarget.value
         });
@@ -49,20 +32,50 @@ function initAudioPlayer(){
 
 window.addEventListener("load", initAudioPlayer);
 
-// <a onclick="myAudioFunction('a');">
-//         <img src="icons/bird.png">
-//     </a>
-//     <a onclick="myAudioFunction('b');">
-//         <img src="icons/cat.png">
-//     </a>
-//     <script>
-//         var aAudio = new Audio('/sounds/birds.mp3');
-//         var bAudio = new Audio('/sounds/cat_purring.mp3');
-//         function myAudioFunction(letter) {
-//             if (letter == 'a') {
-//                 aAudio.play();
-//             } else if (letter == 'b') {
-//                 bAudio.play();
-//             }
-//         }
-//     </script>
+// BACKGROUND CHANGES
+
+icons = document.getElementsByClassName("icon");
+
+for (let i = 0; i < icons.length; i++){
+    let icon = icons[i];
+    let audio = icon.nextElementSibling;
+    let volumeSlider = audio.nextElementSibling;
+
+    rainId = document.getElementById('rain');
+    carId = document.getElementById('car');
+
+    icon.addEventListener("click", function(event) {
+        if (icon === carId){
+            document.body.classList.add("blue")
+        }
+    })
+}
+
+// MUTE BUTTON
+
+// var playing_sounds = [];
+// function muteDocument() {
+// 	if (!is_muted) {
+// 		playing_sounds.length=0;
+// 		is_muted = true;
+// 		document.querySelector('.unmuted').style.display="none";
+// 		document.querySelector('.muted').style.display="inline";
+// 		var all_audio = document.querySelectorAll('audio');
+// 		for (var i = 0; i < all_audio.length; i++) {
+// 			if(!all_audio[i].paused){
+// 				playing_sounds.push([all_audio[i],all_audio[i].volume]);
+// 			}	
+// 		}
+// 		playing_sounds.forEach(function (sound) {
+// 			sound[0].volume=0;
+// 		});
+// 	}
+// 	else{
+// 		is_muted = false;
+// 		document.querySelector('.unmuted').style.display="inline";
+// 		document.querySelector('.muted').style.display="none";
+// 		playing_sounds.forEach(function (sound) {
+// 			sound[0].volume=sound[1];
+// 		});
+// 	}
+// }

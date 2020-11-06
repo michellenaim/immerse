c1d822f1edae2430daa5c38da0c335b1ff0fde26
michellenaim <michellenaim@gmail.com>
2020-11-06 15:04:28 -0800

    Add description

--- sounds.js
+++ sounds.js
@@ -1,5 +1,20 @@
 let classes = []
 
+let colors = {
+    'rain': [230,230,230],
+    'ocean': [46,89,132],
+    'thunder': [159,159,159],
+    'fire': [254,225,209],
+    'lake': [209,251,255],
+    'leaf': [214,255,209],
+    'river': [209,255,239],
+    'bird': [255,253,209],
+    'wind': [216,255,251],
+    'cat': [255,220,248],
+    'night': [233,209,255],
+    'car': [255,195,196],
+}
+
 function audioPlayer(){
     
     let icons = document.getElementsByClassName("icon");
@@ -20,9 +35,11 @@ function audioPlayer(){
                 volumeSlider.classList.add('playing')
 
                 // add different background colors
-                document.body.className = icon.id
                 classes.push(icon.id);
-                console.log(classes);
+
+                rgbString = computeAverage(classes);
+                document.body.style = rgbString
+
             } else {
                 audio.pause();
                 icon.children[0].classList.remove('playing')
@@ -30,18 +47,13 @@ function audioPlayer(){
                 
                 let index = classes.indexOf(icon.id);
                 classes.splice(index, 1);
-                console.log(classes)
-                if (classes.length === 0) {
-                    document.body.className = 'default'
-                } else {
-                    document.body.className = classes[classes.length - 1]
-                }
+                rgbString = computeAverage(classes);
+                document.body.style = rgbString
             }
         });
 
         // SET UP SLIDER
         volumeSlider.addEventListener('input', function(event) {
-            console.log(event.currentTarget.value)
             audio.volume = event.currentTarget.value
         });
 
@@ -87,10 +99,35 @@ function audioPlayer(){
             audio.pause();	
             icon.children[0].classList.remove('playing')
             volumeSlider.classList.remove('playing')
-            document.body.classList.remove(icon.id)
+            classes = []
+            document.body.style = 'background-color: rgb(204, 219, 234)'
         }
     })
 
+}
+
+
+// RGB Averages
+function computeAverage(classes) {
+    if (classes.length === 0 ) {
+        return 'background-color: rgb(204, 219, 234)'
+    }
+
+    let count = 0;
+    let rgb = [0,0,0]
+
+    for (let i = 0; i < classes.length; i++) {
+        count += 1
+        rgb[0] += colors[classes[i]][0]
+        rgb[1] += colors[classes[i]][1]
+        rgb[2] += colors[classes[i]][2]
+    }
+
+    rgb[0] = rgb[0] / count
+    rgb[1] = rgb[1] / count
+    rgb[2] = rgb[2] / count
+
+    return `background-color: rgb(${rgb[0]},${rgb[1]},${rgb[2]})`
 }
 
 
